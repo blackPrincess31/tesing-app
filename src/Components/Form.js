a27333f4ed4679b7872a10ff633b7daf1c73a38b
blackPrincess31 <katesavchenko31@gmail.com>
2022-02-27 18:16:21 +0300

    работает, но

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,98 +2,163 @@ import React, { useState } from 'react';
 import Post from './Post'
 
 function Form() {
-   
+
     const [value, setValue] = useState('');
-    const [tag, setTag] = useState('');
+    // const [tag, setTag] = useState('');
     const [data, setData] = useState([]);
-    const [note, setNote] = useState([]);
-    const [json, setJson] = useState(null);
+    // const [note, setNote] = useState([]);
+    // const [json, setJson] = useState(null);
+    const [editIndex, setEditIndex] = useState(null);
 
-    const handleActive =item =>
-    {
-        setValue(item);
-    };
 
-    const noteChange = e => {
-        setTag(e.target.value);
-    };
-
-    const searchTag = e => {
+    function add(e) {
         e.preventDefault();
-        
-        // let indexOf_tag = data.findIndex(i => {i.indexOf(tag) !==-1});
-        // data.unshift(data[indexOf_tag]);
-        // data.splice(indexOf_tag,1,1);
-        data.find (i => {i.indexOf(tag)});
-        setData(data)
-    };
+        setData([...data, value]);
 
-   const handleChange = e => {
+    }
+
+    function inputValue(e) {
         setValue(e.target.value);
-        let val = value.split(/(#[a-z\d-]+)/ig);
-        for (let i = 0; i < val.length; i++) {
-            if (val[i].charAt(0) === "#") {
-                let array = [];
-                array.push(val[i]);
-                setNote(array);
-            }
+    }
+
+    function remove(index) {
+        setData([...data.slice(0, index), ...data.slice(index + 1)]);
+    }
+
+    function changeValue(e) {         // изменение содержания поля input
+        setValue(e.target.value);
+    }
+
+    const edit = ({ value, setValue }) => {
+    const onChange = (e) => setValue(e.target.value);
+    return (
+        <input
+          type="text"
+          aria-label="Field name"
+          value={value}
+          onChange={onChange}
+        />
+      )
+    }
+
+
+        //  function edit(e) {  
+        //                                         // редактирование элемента
+        //     setValue(
+        //        [
+        //           ...data.slice(0, editIndex), 
+        //           e.target.value,
+        //           ...data.slice(editIndex + 1)
+        //        ]
+        //     ); 
+        //     setEditIndex(null);
+        //  }
+
+
+        function changeValue(e) {            // изменение содержания поля input
+            setValue(e.target.value);
         }
-    };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        let myJson = {
-            data: data,
-            note: note
+        function handleActive(item) {
+            setValue(item);
         };
-        setData(data.concat(value));
-        setJson(JSON.stringify(myJson));
-    };
 
-    const delPost = index => {
-        let arr = data;
-        arr.splice(index,1);
-        setData(arr);
-    };
 
-    const delHashtag = index => {
-        let tag = note;
-        let val = value;
-        let del = tag.splice(index,1);
-        let clearTag = val.substring(0, val.length - 1).replace(del,'');
-        setNote(tag);
-        setValue(clearTag);
-    };
+        // const handleActive =item =>
+        // {
+        //     setValue(item);
+        // };
 
-    const edit = index => {
-        let val = value;
-        let arr = data;
-        arr.splice(index,1,val);
-        setData(arr);
-    };
+        //     const noteChange = e => {
+        //         setTag(e.target.value);
+        //     };
 
-   
+        //     const searchTag = e => {
+        //         e.preventDefault();
+
+        //         // let indexOf_tag = data.findIndex(i => {i.indexOf(tag) !==-1});
+        //         // data.unshift(data[indexOf_tag]);
+        //         // data.splice(indexOf_tag,1,1);
+        //         data.find (i => {i.indexOf(tag)});
+        //         setData(data)
+        //     };
+
+        //    const handleChange = e => {
+        //         setValue(e.target.value);
+        //         let val = value.split(/(#[a-z\d-]+)/ig);
+        //         for (let i = 0; i < val.length; i++) {
+        //             if (val[i].charAt(0) === "#") {
+        //                 let array = [];
+        //                 array.push(val[i]);
+        //                 setNote(array);
+        //             }
+        //         }
+        //     };
+
+        //     const handleSubmit = e => {
+        //         e.preventDefault();
+        //         let myJson = {
+        //             data: data,
+        //             note: note
+        //         };
+        //         setData(data.concat(value));
+        //         setJson(JSON.stringify(myJson));
+        //     };
+
+        //     const delPost = index => {
+        //         let arr = data;
+        //         arr.splice(index,1);
+        //         setData(arr);
+        //     };
+
+        //     const delHashtag = index => {
+        //         let tag = note;
+        //         let val = value;
+        //         let del = tag.splice(index,1);
+        //         let clearTag = val.substring(0, val.length - 1).replace(del,'');
+        //         setNote(tag);
+        //         setValue(clearTag);
+        //     };
+
+        //     const edit = index => {
+        //         let val = value;
+        //         let arr = data;
+        //         arr.splice(index,1,val);
+        //         setData(arr);
+        //     };
+
+
         return (
             <div>
-               <Post
-                   value={value}
-                   data={data}
-                   note={note}
-                   tag={tag}
-                   handleChange={handleChange}
-                   delHashtag={delHashtag}
-                   delPost={delPost}
-                   edit={edit}
-                   handleSubmit={handleSubmit}
-                   handleActive={handleActive}
-                   searchTag={searchTag}
-                   noteChange={noteChange}
-               />
+                <Post
+                    value={value}
+                    data={data}
+                    add={add}
+                    inputValue={inputValue}
+                    remove={remove}
+                    changeValue={changeValue}
+                    edit={edit}
+                    handleActive={handleActive}
+                    editIndex={editIndex}
+                    setEditIndex={setEditIndex}
+                //    note={note}
+                //    tag={tag}
+                //    handleChange={handleChange}
+                //    delHashtag={delHashtag}
+                //    delPost={delPost}
+                //    edit={edit}
+                //    handleSubmit={handleSubmit}
+                //    handleActive={handleActive}
+                //    searchTag={searchTag}
+                //    noteChange={noteChange}
+                />
             </div>
         );
-   }
 
-   export default Form;
 
-    
+    }
+
+    export default Form;
+
+
 
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -5,29 +5,36 @@ function Post(props) {
     return (
         <div className="Post">
             <form className="someText">
-                <textarea value={props.value} onChange={props.handleChange} placeholder="Введите техт"></textarea>
-                <button className="save" onClick={props.handleSubmit}>Сохранить</button>
+                {/* <textarea value={props.value} onChange={props.handleChange} placeholder="Input text"></textarea>
+                <button className="save" onClick={props.handleSubmit}>ADD</button> */}
+                <input value={props.value} onChange={props.changeValue} />
+                <button onClick={props.add}>Добавить элемент</button>
             </form>
             <form className="searchTag">
-                <input placeholder="Искать заметку по тегу" value={props.tag} onChange={props.noteChange} />
-                <button className="tagButton" onClick={props.searchTag}>Искать</button>
+                {/* <input placeholder="Искать заметку по тегу" value={props.tag} onChange={props.noteChange} />
+                <button className="tagButton" onClick={props.searchTag}>Искать</button> */}
             </form>
             <ul className='listBox'>
                 {props.data.length > 0 ? props.data.map((item, index) =>
                     <div key={index} className='notes'>
-                        <li onClick={() => props.handleActive(item)} className='someNote'>{item}</li>
+                        {/* <li onClick={() => props.handleActive(item)} className='someNote'>{item}</li>
                         <button onClick={() => props.edit(index)} className='changeNote'>Изменить заметку</button>
-                        <button onClick={() => props.delPost(index)} className='delNote'>Удалить заметку</button>
+                        <button onClick={() => props.delPost(index)} className='delNote'>Удалить заметку</button> */}
+                        <li onClick={() => props.handleActive(item)} className='someNote'>
+                        {item}
+                        </li>
+                        <button onClick={() => props.edit } value ={props.value} className='changeNote'>Изменить заметку</button>
+                        <button onClick={() => props.remove(index)} className='delNote'>Удалить заметку</button>
                     </div>
                 ) : null}
             </ul>
             <ul className='containerNote'>
-                {props.note.length > 0 ? props.note.map((item, index) =>
+              {/* {props.note.length > 0 ? props.note.map((item, index) =>
                     <div key={index} className='tags'>
                         <li className='someTag'>{item}</li>
                         <button onClick={() => props.delHashtag(index)} className='delTag'>Удалить тэг</button>
                     </div>
-                ) : null}
+                ) : null}  */}
             </ul>
         </div>
     )
@@ -35,17 +42,28 @@ function Post(props) {
 
 Post.propTypes = {
     data: PropTypes.array,
-    note: PropTypes.array,
-    searchTag: PropTypes.func,
+   changeValue: PropTypes.func,
+    
     edit: PropTypes.func,
-    handleChange: PropTypes.func,
-    delHashtag: PropTypes.func,
-    delPost: PropTypes.func,
-    handleSubmit: PropTypes.func,
-    noteChange: PropTypes.func,
-    json: PropTypes.object,
+    
+    remove: PropTypes.func,
+    add: PropTypes.func,
+   
+  
     value: PropTypes.string,
-    tag: PropTypes.string
+    
+    // data: PropTypes.array,
+    // note: PropTypes.array,
+    // searchTag: PropTypes.func,
+    // edit: PropTypes.func,
+    // handleChange: PropTypes.func,
+    // delHashtag: PropTypes.func,
+    // delPost: PropTypes.func,
+    // handleSubmit: PropTypes.func,
+    // noteChange: PropTypes.func,
+    // json: PropTypes.object,
+    // value: PropTypes.string,
+    // tag: PropTypes.string
 };
 
-export default Post ;
+export default Post;
